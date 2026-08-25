bc50e906fc57f26f26569532eaf0b54e5b0c2221
Maryam Faryad <maryamrajput0013@gmail.com>
2026-08-25 12:51:35 -0700

    Time: 11 ms (6.82%) | Memory: 9.7 MB (49.53%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> m1;
+        map<char, int> m2;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (m1.find(s[i]) != m1.end()) {
+                m1[s[i]] = m1[s[i]] + 1;
+            } else {
+                m1[s[i]] = 1;
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (m2.find(t[i]) != m2.end()) {
+                m2[t[i]] = m2[t[i]] + 1;
+            } else {
+                m2[t[i]] = 1;
+            }
+        }
+      // return true or false
+        return m1 == m2;
+    }
+};
